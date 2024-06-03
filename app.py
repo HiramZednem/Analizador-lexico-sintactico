30a23a95ebf10d857cba2863534a3d126d1de015
HiramZednem <hirammendez000@gmail.com>
2024-06-03 08:36:17 -0600

    structure

---
+++ app.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask, request, render_template
+from lexer import lexical_analysis
+from parser import syntactic_analysis
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    tokens = []
+    syntax_result = ''
+    if request.method == 'POST':
+        code = request.form['code']
+        tokens = lexical_analysis(code)
+        syntax_result = syntactic_analysis(tokens)
+    return render_template('index.html', tokens=tokens, syntax_result=syntax_result)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001, host='0.0.0.0')
---
+++ lexer.py
@@ -0,0 +1,35 @@
+# lexer.py
+import re
+
+def lexical_analysis(code):
+    tokens = []
+    token_specification = [
+        ('FOR', r'\bfor\b'),
+        ('INT', r'\bint\b'),
+        ('PRINTLN', r'\bprintln\b'),
+        ('SYSTEM', r'\bsystem\.out\.println\b'),
+        ('PARENIZQ', r'\('),
+        ('PARENDER', r'\)'),
+        ('LLAVEIZQ', r'\{'),
+        ('LLAVEDER', r'\}'),
+        ('PUNTOCOMA', r';'),
+        ('NUM', r'\d+'),
+        ('ID', r'[A-Za-z_]\w*'),
+        ('OP', r'[+]|<=|<|>|>=|=='),
+        ('EQ', r'='),
+        ('STRING', r'".*"'),
+        ('COMA', r','),
+        ('SPACE', r'\s+'),
+        ('UNKNOWN', r'.')
+    ]
+    tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
+    line_num = 1
+    line_start = 0
+    for mo in re.finditer(tok_regex, code):
+        kind = mo.lastgroup
+        value = mo.group()
+        column = mo.start() - line_start
+        if kind == 'SPACE':
+            continue
+        tokens.append((kind, value, line_num, column))
+    return tokens
